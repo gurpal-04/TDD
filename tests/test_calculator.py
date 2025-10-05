c039fb9a246b174d513d90e91e6e9c162cbcd14f
gurpal-04 <gurpal.py@gmail.com>
2025-10-05 14:02:40 +0530

    test: add failing test for single number input

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,3 +2,6 @@ from string_calculator.calculator import add
 
 def test_returns_zero_for_empty_string():
     assert add("") == 0
+
+def test_returns_single_number_value():
+    assert add("1") == 1
