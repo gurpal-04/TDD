14b2e8664e7738ac398615dfda7e8fa95b8171ec
gurpal-04 <gurpal.py@gmail.com>
2025-10-05 14:23:15 +0530

    refactor: simplify number splitting and summing

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,8 +1,4 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-
-    if "," in numbers:
-        n,m = numbers.split(",")
-        return int(n) + int(m)
-    return int(numbers)
+    return sum(int(n) for n in numbers.split(","))
