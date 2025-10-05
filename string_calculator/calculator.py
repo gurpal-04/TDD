1e6f3660949c87f1196844c12dbd4d073582a965
gurpal-04 <gurpal.py@gmail.com>
2025-10-05 14:20:51 +0530

    feat: handled two comma-separated numbers

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,4 +1,8 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+
+    if "," in numbers:
+        n,m = numbers.split(",")
+        return int(n) + int(m)
     return int(numbers)
