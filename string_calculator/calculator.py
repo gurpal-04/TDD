6da45cf0563cb1ce5ed150de1c36fe67d44df269
gurpal-04 <gurpal.py@gmail.com>
2025-10-05 15:01:13 +0530

    feat: support custom delimiter syntax

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,5 +1,12 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+
+    if numbers.startswith("//"):
+        delimiter = numbers[2]
+        numbers = numbers[4:]
+        
+        return sum(int(n) for n in numbers.split(delimiter))
+
     clean = numbers.replace("\n", ",")
     return sum(int(n) for n in clean.split(","))
