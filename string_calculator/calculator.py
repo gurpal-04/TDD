a5b940008ec27252deaed2aabce12e7decab547f
gurpal-04 <gurpal.py@gmail.com>
2025-10-05 15:44:51 +0530

    feat: raise exception listing all negative numbers

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,11 +1,15 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-        
+
     delimiter = "\n"
     if numbers.startswith("//"):
         delimiter = numbers[2]
         numbers = numbers[4:]
     
     clean = numbers.replace(delimiter, ",")
+    negative_numbers = [n for n in clean.split(",") if int(n) < 0]
+    
+    if negative_numbers:
+        raise Exception(f"negative numbers not allowed {','.join(negative_numbers)}")
     return sum(int(n) for n in clean.split(","))
